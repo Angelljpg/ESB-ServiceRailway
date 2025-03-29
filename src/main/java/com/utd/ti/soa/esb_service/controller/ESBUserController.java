f459190d9242c1d438ceaae005ddbe75260839ed
ErnestoOrtega4ABIS <ernestoortega1423@gmail.com>
2025-03-28 23:06:27 -0600

    init commit changes

--- src/main/java/com/utd/ti/soa/esb_service/controller/ESBUserController.java
+++ src/main/java/com/utd/ti/soa/esb_service/controller/ESBUserController.java
@@ -20,10 +20,10 @@ import com.utd.ti.soa.esb_service.utils.Auth;
 @RequestMapping("/api/v1/esb")
 public class ESBUserController {
 
-    private final WebClient webClient = WebClient.create("https://usersrailway-production-d953.up.railway.app");
+    private final WebClient webClient = WebClient.create("https://usersrailway-production.up.railway.app");
     private final Auth auth = new Auth();
 
-    
+
     @PostMapping("/user")
     public ResponseEntity<String> createUser(@RequestBody User user, @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
         System.out.println("Request Body: " + user);
